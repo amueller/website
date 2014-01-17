b3cfa9fd0d6d58b25c6128269a128d31e5b7984c
Jan N. van Rijn <janvanrijn@gmail.com>
2014-01-17 16:03:30 +0100

    bugfix

--- openml_OS/views/pages/rest_api/xml/implementation-exists.tpl.php
+++ openml_OS/views/pages/rest_api/xml/implementation-exists.tpl.php
@@ -1,4 +1,4 @@
-<oml:upload_implementation xmlns:oml="http://openml.org/openml">
+<oml:implementation_exists xmlns:oml="http://openml.org/openml">
 	<oml:exists><?php echo $exists; ?></oml:exists>
 	<oml:id><?php echo $id; ?></oml:id>
-</oml:upload_implementation>
+</oml:implementation_exists>
