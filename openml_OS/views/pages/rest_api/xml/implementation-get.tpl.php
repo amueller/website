bfbe765d92267a6525811f9d642cb0411e3f6e8b
janvanrijn <janvanrijn@gmail.com>
2014-01-19 16:22:07 +0100

    implementation get xml

--- openml_OS/views/pages/rest_api/xml/implementation-get.tpl.php
+++ openml_OS/views/pages/rest_api/xml/implementation-get.tpl.php
@@ -1,39 +1,40 @@
 <?php 
 $mapping = array(
-	'id' => 'id',
-	'uploader' => 'uploader',
-	'name' => 'name',
-	'version' => 'version',
-	'description' => 'description',
-	'creator' => 'creator',
-	'contributor' => 'contributor',
-	'upload_date' => 'uploadDate',
-	'licence' => 'licence',
-	'language' => 'language',
-	'full_description' => 'fullDescription',
-	'installation_notes' => 'installationNotes',
-	'dependencies' => 'dependencies',
-	'bibliographical_reference' => 'bibliographicalReference',
-	'parameters' => 'parameterSetting',
-	'components' => 'components',
-	'source_url' => 'sourceUrl',
-	'binary_url' => 'binaryUrl',
-	'source_format' => 'sourceFormat',
-	'binary_format' => 'binaryFormat',
-	'source_md5' => 'sourceMd5',
-	'binary_md5' => 'binaryMd5',
-	); ?>
+  'id' => 'id',
+  'uploader' => 'uploader',
+  'name' => 'name',
+  'version' => 'version',
+  'description' => 'description',
+  'creator' => 'creator',
+  'contributor' => 'contributor',
+  'upload_date' => 'uploadDate',
+  'licence' => 'licence',
+  'language' => 'language',
+  'full_description' => 'fullDescription',
+  'installation_notes' => 'installationNotes',
+  'dependencies' => 'dependencies',
+  'bibliographical_reference' => 'bibliographicalReference',
+  'parameters' => 'parameterSetting',
+  'components' => 'components',
+  'source_url' => 'sourceUrl',
+  'binary_url' => 'binaryUrl',
+  'source_format' => 'sourceFormat',
+  'binary_format' => 'binaryFormat',
+  'source_md5' => 'sourceMd5',
+  'binary_md5' => 'binaryMd5',
+ ); ?>
 
 <oml:implementation xmlns:oml="http://openml.org/openml">
-	<?php 
-	foreach( $mapping as $key => $value ) {
-		if( property_exists( $source, $value ) ){
-			if( is_array( $source->$value ) ) {
-				if( count( $source->$value ) > 0 )
-					sub_xml( 'implementation-get.' . $value, array( 'source' => $source->$value ), false );
-			} elseif( $source->$value !== false && $source->$value !== null ) { 
-				echo '<oml:'.$key.'>'.htmlentities($source->$value).'</oml:'.$key.'>';
-			}
-		}
-	}?>
+  <?php 
+  foreach( $mapping as $key => $value ) {
+    if( property_exists( $source, $value ) ){
+      if( is_array( $source->$value ) ) {
+        if( count( $source->$value ) > 0 ) {
+          sub_xml( 'implementation-get.' . $value, array( 'source' => $source->$value ), false );
+        }
+      } elseif( $source->$value !== false && $source->$value !== null ) { 
+        echo '<oml:'.$key.'>'.htmlentities($source->$value).'</oml:'.$key.'>' . "\n";
+      }
+    }
+  }?>
 </oml:implementation>
