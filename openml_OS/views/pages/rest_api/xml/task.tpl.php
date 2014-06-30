5efc083654dc1398d826fffed9fbd6139f941d49
Jan N. van Rijn <janvanrijn@gmail.com>
2014-06-30 18:08:39 +0200

    bugfix

--- openml_OS/views/pages/rest_api/xml/task.tpl.php
+++ openml_OS/views/pages/rest_api/xml/task.tpl.php
@@ -2,8 +2,11 @@
 	<oml:task_id><?php echo $task->task_id;?></oml:task_id>
 	<oml:task_type><?php echo $task_type->name;?></oml:task_type>
 	<?php foreach( $parsed_io as $key => $item ): ?>
-  <oml:input name="<?php echo $key; ?>">
+    <?php // TODO: hack. fix this!!
+      $inout = $this->Task_type_inout->getWhere( 'ttid = ' . $task_type->ttid . ' AND name = "' . $key . '"' ); 
+    ?>
+  <oml:<?php echo $inout[0]->io; ?> name="<?php echo $key; ?>">
 		<?php echo $item; ?>
-  </oml:input>
+  </oml:<?php echo $inout[0]->io; ?>>
 	<?php endforeach; ?>
 </oml:task>
