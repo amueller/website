b4a1c12330e9743c59530eac1336c7590d7b2711
janvanrijn <janvanrijn@gmail.com>
2014-07-25 17:49:54 +0200

    api html output

--- openml_OS/views/pages/rest_api/xml/task-types.tpl.php
+++ openml_OS/views/pages/rest_api/xml/task-types.tpl.php
@@ -3,7 +3,7 @@
 	<oml:task_type>
 		<oml:id><?php echo $tt->ttid; ?></oml:id>
 		<oml:name><?php echo $tt->name; ?></oml:name>
-		<oml:description><?php echo $tt->description; ?></oml:description>
+		<oml:description><?php echo htmlspecialchars( $tt->description ); ?></oml:description>
 		<oml:creator><?php echo $tt->creator; ?></oml:creator>
 	</oml:task_type>
 	<?php endforeach;?> 
