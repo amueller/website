633c736d55010a4f0ab8e41fa4770c4ba6cd4e64
janvanrijn <janvanrijn@gmail.com>
2014-08-13 11:52:19 +0200

    removed elastis search auto indexer for now

--- openml_OS/controllers/rest_api.php
+++ openml_OS/controllers/rest_api.php
@@ -51,6 +51,8 @@ class Rest_api extends CI_Controller {
     
     // helper
     $this->load->helper('api');
+
+    $this->load->library('elasticSearch');
     
     // paths
     $this->data_folders = array(
@@ -487,7 +489,7 @@ class Rest_api extends CI_Controller {
     $this->db->trans_complete();
     
     // add to elastic search index. 
-    $this->ElasticSearch->index('data', $dataset->did); 
+    $this->elasticsearch->index('data', $dataset->did); 
     
     if( $success ) {
       $this->_xmlContents( 'data-qualities-upload', array( 'did' => $dataset->did ) );
@@ -634,7 +636,7 @@ class Rest_api extends CI_Controller {
      * * * */
     $id = $this->Dataset->insert( $dataset );
     // update elastic search index. 
-    $this->ElasticSearch->index('data', $id);
+    $this->elasticsearch->index('data', $id);
     if( ! $id ) {
       $this->_returnError( 134 );
       return;
@@ -1227,7 +1229,7 @@ class Rest_api extends CI_Controller {
     }
     
     // add to elastic search index. 
-    $this->ElasticSearch->index('run', $run->rid); 
+    $this->elasticsearch->index('run', $run->rid); 
     
     /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
      * Now the stuff that needs to be done for the special     *
@@ -1357,7 +1359,7 @@ class Rest_api extends CI_Controller {
     $this->db->trans_complete();
     
     // update elastic search index. 
-    $this->ElasticSearch->index('run', $run_id ); 
+    $this->elasticsearch->index('run', $run_id ); 
     
     $this->_xmlContents( 'run-evaluate', array( 'run_id' => $run_id ) );
   }
--- openml_OS/models/task.php
+++ openml_OS/models/task.php
@@ -77,7 +77,7 @@ class Task extends Database_write {
     $this->Task_inputs->insert_batch( $to_insert );
     // add to elastic search index. 
     foreach( $result as $r ) {
-      $this->ElasticSearch->index('task', $r ); 
+      //$this->elasticsearch->index('task', $r ); 
     }
     
     return $result;
