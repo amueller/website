7f321300095261445745d3af7440a2957c0599cc
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2014-08-14 17:53:51 +0200

    updated emails

--- openml_OS/views/email/activate.tpl.php
+++ openml_OS/views/email/activate.tpl.php
@@ -1,6 +1,9 @@
 <html>
 <body>
-	<h1>Activate account for <?php echo $identity;?></h1>
-	<p>Please click this link to <?php echo anchor('frontend/page/profile_activate/id/'. $id .'/code/'. $activation, 'Activate Your Account');?>.</p>
+	<h3>Welcome to OpenML</h3>
+	<p>Before you go out and play on OpenML, please verify your email address.</p>
+	<p>Click this link to <?php echo anchor('frontend/page/profile_activate/id/'. $id .'/code/'. $activation, 'Activate Your Account');?>.</p>
+
+	<p>If you did not create an OpenML account using this address, please contact us at openmachinelearning@gmail.com.</p>
 </body>
 </html>
