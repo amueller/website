64ac5aa136b9c3c24490f5724c3dc13b9af52e62
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2014-08-18 00:55:25 +0200

    bugfix

--- openml_OS/views/output_main.php
+++ openml_OS/views/output_main.php
@@ -2,8 +2,6 @@
   $info = explode('/', $_SERVER['REQUEST_URI']);
   $run_id = $info[array_search('r',$info)+1];
   $outputname = $info[array_search('output',$info)+1];
-  echo $run_id;
-  echo $outputname;
 
   $getParams = array();
   $getParams['index'] = 'openml';
@@ -11,10 +9,8 @@
   $getParams['id']    = $run_id;
   $searchclient = $this->searchclient->get($getParams);
   $url = $searchclient['_source']['output_files'][$outputname];
-  print_r($searchclient['_source']['output_files']);
-  print_r($url);
   $urlparts = explode('/', $url);
-  $outputid = $info[array_search('download',$urlparts)+1];
+  $outputid = $urlparts[array_search('download',$urlparts)+1];
   $file = 'data/download/'.$outputid;
 
   if(file_exists($file))
@@ -25,6 +21,6 @@
 		exit;
 	}
   else{
-	echo 'File available at '. $file;
-	}
+	echo "Oops, something went wrong. File not found.";
+}
 ?>
