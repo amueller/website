d0623b22da22bdaa9fc20569c96ba18f5d706b38
janvanrijn <janvanrijn@gmail.com>
2014-08-26 15:25:43 +0200

    added functionality

--- openml_OS/config/openml_mappings.php
+++ openml_OS/config/openml_mappings.php
@@ -6,10 +6,22 @@ $config['data_tables'] = array(
 
 $config['xml_fields_dataset'] = array(
   'string'  => array(
-    'description','format','collection_date','language','licence',
-    'default_target_attribute','row_id_attribute','version_label',
-    'citation','visibility','original_data_url','paper_url','md5_checksum'),
-  'csv'     => array('creator','contributor',),
+     0 => 'name',
+     1 => 'description',
+     2 => 'format',
+     5 => 'collection_date',
+     6 => 'language',
+     7 => 'licence',
+     8 => 'default_target_attribute',
+     9 => 'row_id_attribute',
+    10 => 'version_label',
+    11 => 'citation',
+    12 => 'visibility',
+    13 => 'original_data_url',
+    14 => 'paper_url'),
+  'csv'     => array(
+     3 => 'creator',
+     4 => 'contributor'),
   'array'   => array(),
   'plain'   => array()
 );
--- openml_OS/libraries/DataOverview.php
+++ openml_OS/libraries/DataOverview.php
@@ -15,5 +15,36 @@ class DataOverview {
     
     return $this->CI->load->view('library_templates/data_overview_default', $variables, true);
   }
-
+  
+  public function generate_xml( $root, $tag_configuration ) {
+    $xml = new SimpleXMLElement('<oml:'.$root.' xmlns:oml="http://openml.org/openml"/>');
+    
+    // first obtain the indices
+    $indices = array();
+    foreach( $tag_configuration as $key => $value ) {
+      $indices = array_merge( $indices, array_keys( $value ) );
+    }
+    sort( $indices );
+    
+    foreach( $indices as $index ) {
+      foreach( $tag_configuration as $tag_type => $tags ) {
+        foreach( $tags as $tag_index => $tag_name ) {
+          if( $tag_index == $index ) {
+            if( $this->CI->input->post( $tag_name ) ) {
+              if( $tag_type == 'csv' ) {
+                $values_exploded = explode( ',', $this->CI->input->post($tag_name) );
+                foreach( $values_exploded as $value ) {
+                  $xml->addChild('oml:'.$tag_name, $value);
+                }
+              } else { // TODO: add support for plain and array.
+                $value = $this->CI->input->post($tag_name);
+                $xml->addChild('oml:'.$tag_name, $value);
+              }
+            }
+          }
+        }
+      }
+    }
+    return $xml->asXML();
+  }
 }
