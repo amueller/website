2394e81cb8e12ac843697bfc450c704d59a910a3
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2014-09-02 23:50:25 +0200

    tweak

--- openml_OS/views/pages/frontend/d/subpage/dataset.php
+++ openml_OS/views/pages/frontend/d/subpage/dataset.php
@@ -20,14 +20,15 @@
 
     <div class="datainfo">
        <i class="fa fa-cc"></i> <?php $l = $this->licences[$this->record->{'licence'}]; echo '<a href="'.$l['url'].'">'.$l['name'].'</a>'; ?> 	    
-       <?php if($this->record->{'citation'}){ ?>
-	    <i class="fa fa-book"></i> Please cite: <?php echo $this->record->{'citation'} ?>
-       <?php } ?>
        <?php if($this->record->{'paper_url'}){ ?>
 	    <i class="fa fa-book"></i> <a href="<?php echo $this->record->{'paper_url'} ?>">Original paper</a>
        <?php } ?>
        <i class="fa fa-eye-slash"></i> Viewable by <?php echo strtolower($this->record->{'visibility'}); ?> 
        <i class="fa fa-cloud-upload"></i> Uploaded by <a href="<?php echo $this->uploader_id; ?>"><?php echo $this->record->{'uploader'} ?></a> on <td><?php echo explode(" ",$this->record->{'upload_date'})[0];?>
+       <?php if($this->record->{'citation'}){ ?>
+	    <br><i class="fa fa-book"></i> Please cite: <?php echo $this->record->{'citation'} ?>
+       <?php } ?>
+
     </div>
   </div>
   <div class="col-xs-12">
