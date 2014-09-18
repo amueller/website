b641a0ef7800b59cdd74e26e9ece8fec160a3019
janvanrijn <janvanrijn@gmail.com>
2014-09-18 16:18:37 +0200

    bugfix

--- openml_OS/views/pages/backend/meta_dataset/pre.php
+++ openml_OS/views/pages/backend/meta_dataset/pre.php
@@ -7,7 +7,7 @@ if (!$this->ion_auth->logged_in()) {
 $this->measures = array("predictive_accuracy", "build_cpu_time", "area_under_roc_curve");
 
 $sql = 'SELECT `m`.`id`, `m`.`request_date`, `m`.`datasets`, `m`.`flows`, `m`.`functions`, ' .
-       'IF(`f`.`id` IS NOT NULL, CONCAT("<a href=\"'.DATA_URL.'", `f`.`id`, "/", `f`.`filename_original`, "\" target=\"_blank\"><i class=\"fa fa-file-excel-o\"></i></a>"), "") AS `download` ' .
+       'IF(`f`.`id` IS NOT NULL, CONCAT("<a href=\"'.DATA_URL.'download/", `f`.`id`, "/", `f`.`filename_original`, "\" target=\"_blank\"><i class=\"fa fa-file-excel-o\"></i></a>"), "") AS `download` ' .
        'FROM `meta_dataset` `m` LEFT JOIN `file` `f` ON `m`.`file_id` = `f`.`id` ' .
        'WHERE user_id = ' . $this->ion_auth->get_user_id() . ' ' .
        'ORDER BY request_date DESC; ';
