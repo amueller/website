d189cc4f4f2b3f51c6038f17d5f09b1453e794aa
janvanrijn <janvanrijn@gmail.com>
2014-09-26 16:39:33 +0200

    added username/password

--- openml_OS/controllers/api_splits.php
+++ openml_OS/controllers/api_splits.php
@@ -45,7 +45,8 @@ class Api_splits extends CI_Controller {
     // TODO: very important. sanity check input
     $testset_str = array_key_exists('custom_testset', $values) && is_cs_natural_numbers($values['custom_testset']) ?  '-test "' . $values['custom_testset'] . '"' : '';
     
-    $command = 'java -jar '.$this->evaluation.' -f "generate_folds" -d "'.$dataset->url.'" -e "'.$epstr.'" -c "'.$values['target_feature'].'" -r "'.safe($dataset->row_id_attribute).'" ' . $testset_str; 
+    // TODO: remove admin account from bitbucket, set in settings
+    $command = 'java -jar '.$this->evaluation.' -f "generate_folds" -d "'.$dataset->url.'" -e "'.$epstr.'" -c "'.$values['target_feature'].'" -r "'.safe($dataset->row_id_attribute).'" ' . $testset_str . " -config 'server=http://www.openml.org/;username=API_USERNAME;password=API_PASSWORD'"; 
     if( $md5 ) $command .= ' -m';
     $this->Log->cmd( 'API Splits::get(' . $task_id . ')', $command );
     
