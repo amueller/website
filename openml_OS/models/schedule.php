89e033e68edb34aa75f23250a007c623a1045dec
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2014-10-12 08:23:18 +0200

    fixed scheduling

--- openml_OS/models/schedule.php
+++ openml_OS/models/schedule.php
@@ -9,13 +9,13 @@ class Schedule extends Database_write {
   
   function getJob( $workbench, $task_type ) {
     // quick check
-    $sql = 'SELECT * FROM `schedule` WHERE `dependencies` = "' . $workbench . '" AND `ttid` = "' . $task_type . '" AND last_assigned = NULL limit 0,1;';
+    $sql = 'SELECT * FROM `schedule` WHERE `dependencies` = "' . $workbench . '" AND `active` = "true" AND `ttid` = "' . $task_type . '" AND last_assigned = NULL limit 0,1;';
     
     $res = $this->query( $sql );
 
     // pick up abandoned jobs
     if(is_array($res) == false) {
-	$sql = 'SELECT * FROM `schedule` WHERE `dependencies` = "' . $workbench . '" AND `ttid` = "' . $task_type . '" ORDER BY last_assigned ASC limit 0,1;';
+	$sql = 'SELECT * FROM `schedule` WHERE `dependencies` = "' . $workbench . '" AND `active` = "true" AND `ttid` = "' . $task_type . '" ORDER BY last_assigned ASC limit 0,1;';
 
     	$res = $this->query( $sql );
     }
