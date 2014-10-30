61f8363e2c6cee96e451578ad5ac5097451da35b
janvanrijn <janvanrijn@gmail.com>
2014-10-30 13:45:42 +0100

    bugfix

--- openml_OS/controllers/rest_api.php
+++ openml_OS/controllers/rest_api.php
@@ -1656,7 +1656,7 @@ class Rest_api extends CI_Controller {
     $run->inputData = $this->Run->getInputData( $run->rid ); 
     $run->outputData = $this->Run->getOutputData( $run->rid ); 
     $run->setup = $this->Algorithm_setup->getById( $run->setup );
-    $run->tags = $this->Run_tags->getColumnWhere( 'id = ' $run->rid );
+    $run->tags = $this->Run_tag->getColumnWhere( 'id = ' . $run->rid );
     $run->inputSetting = $this->Input_setting->query('SELECT i.name, s.value from input i, input_setting s where i.id=s.input_id and setup = ' . $run->setup->sid );
     
     $this->_xmlContents( 'run-get', array( 'source' => $run ) );
