abc036996c874966ec83bb7a21844330cd4b3854
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2014-12-08 22:54:28 +0100

    getjob fix

--- openml_OS/models/schedule.php
+++ openml_OS/models/schedule.php
@@ -1,16 +1,16 @@
 <?php
 class Schedule extends Database_write {
-  
+
   function __construct() {
     parent::__construct();
     $this->table = 'schedule';
     $this->id_column = array('sid','task_id');
   }
-  
+
   function getJob( $workbench, $task_type ) {
     // quick check
-    $sql = 'SELECT * FROM `schedule` WHERE `dependencies` = "' . $workbench . '" AND `active` = "true" AND `ttid` = "' . $task_type . '" AND last_assigned = NULL limit 0,1;';
-    
+    $sql = 'SELECT * FROM `schedule` WHERE `dependencies` = "' . $workbench . '" AND `active` = "true" AND `ttid` = "' . $task_type . '" AND last_assigned is NULL limit 0,1;';
+
     $res = $this->query( $sql );
 
     // pick up abandoned jobs
