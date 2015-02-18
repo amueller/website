f5227c5893627be3112834f77512fd4dfc29b3c0
janvanrijn <janvanrijn@gmail.com>
2015-02-18 13:22:48 +1300

    Bournemouth

--- openml_OS/controllers/rest_api.php
+++ openml_OS/controllers/rest_api.php
@@ -202,7 +202,7 @@ class Rest_api extends CI_Controller {
   }
   
   private function _openml_data() {
-    $active = $this->input->get('active_only') ? 'status = "active"' : '1=1';
+    $active = $this->input->get('active_only') ? 'status = "active"' : 'did = did'; // no constraints
     $datasets_res = $this->Dataset->getWhere( $active, 'did' );
     if( is_array( $datasets_res ) == false || count( $datasets_res ) == 0 ) {
       $this->_returnError( 370 );
