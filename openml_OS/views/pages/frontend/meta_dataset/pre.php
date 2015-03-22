d058859c01a91574071ea0f21b032cffd0091bc0
janvanrijn <janvanrijn@gmail.com>
2015-03-22 16:44:12 +1300

    same

--- openml_OS/views/pages/frontend/meta_dataset/pre.php
+++ openml_OS/views/pages/frontend/meta_dataset/pre.php
@@ -134,12 +134,14 @@ if( $_POST || $this->input->get('check') ) {
     // TODO: implementations
     $sql_runs = 
       'SELECT `r`.`task_id`,`r`.`setup` ' .
-      'FROM `run` `r`, `task_inputs` `d` ' .
+      'FROM `run` `r`, `task_inputs` `d`, `task` `t` ' .
       'WHERE `r`.`task_id` = `d`.`task_id` ' . 
       'AND `d`.`input` = "source_data" ' .
+      'AND `t`.`task_id` = `r`.`task_id` ' . 
       (($dataset_ids) ? ('AND `d`.`value` IN (' . implode( ',', $dataset_ids ) . ') ') : '' ) . 
       (($task_ids) ? ('AND `r`.`task_id` IN (' . implode( ',', $task_ids ) . ') ') : '' ) . 
       (($setup_ids) ? ('AND `r`.`setup` IN (' . implode( ',', $setup_ids ) . ') ') : '' ) . 
+      'AND `t`.`ttid` = "'.$task_type.'" ' .
       'GROUP BY `r`.`task_id`, `r`.`setup`;';
     $res_runs = $this->Run->query( $sql_runs );
     $this->data = array();
