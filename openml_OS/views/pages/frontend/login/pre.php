3a15226dafdda5b02efe9645dc801bfc8ce44a3f
janvanrijn <janvanrijn@gmail.com>
2015-05-28 15:29:26 +0200

    redirection

--- openml_OS/views/pages/frontend/login/pre.php
+++ openml_OS/views/pages/frontend/login/pre.php
@@ -1,15 +1,20 @@
 <?php
+
 $this->active = 'profile';
 $this->message = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
 
-
-if( array_key_exists( 'HTTP_REFERER', $_SERVER ) ) {
-  if( startsWith( BASE_URL, $_SERVER['HTTP_REFERER'] ) ) {
-    $this->session->set_flashdata('login_redirect', $_SERVER['HTTP_REFERER'] );
-  } else {
-    $this->session->set_flashdata('login_redirect', false );
+if( $this->session->flashdata('login_redirect') == false ) {
+  if( array_key_exists( 'HTTP_REFERER', $_SERVER ) ) {
+    if( startsWith( $_SERVER['HTTP_REFERER'], BASE_URL ) ) {
+      $this->session->set_flashdata('login_redirect', $_SERVER['HTTP_REFERER'] );
+    } else {
+      $this->session->set_flashdata('login_redirect', false );
+    }
   }
+} else {
+  $this->session->set_flashdata('login_redirect', $this->session->flashdata('login_redirect') );
 }
+
 
 $this->identity = array('name' => 'identity',
 	'id' => 'identity',
