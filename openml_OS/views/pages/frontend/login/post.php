958e4ad59625093bb5a7bb7b2b52c99ac82e7515
janvanrijn <janvanrijn@gmail.com>
2015-05-28 11:57:09 +0200

    redirect after login

--- openml_OS/views/pages/frontend/login/post.php
+++ openml_OS/views/pages/frontend/login/post.php
@@ -2,11 +2,11 @@
 if ($this->ion_auth->login($this->input->post('identity'), $this->input->post('password'), false))
 {
 	$this->session->set_flashdata('message', $this->ion_auth->messages());
-	redirect('frontend');
+	redirect('home');
 }
 else
 {
 	$this->session->set_flashdata('message', $this->ion_auth->errors());
-	redirect('frontend/page/login');
+	redirect('login');
 }
 ?>
