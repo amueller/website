48dcb5f74d4c4f739ed585c3d292069206ed0597
janvanrijn <janvanrijn@gmail.com>
2015-06-29 21:09:40 +0200

    rm plugin

--- openml_OS/views/pages/frontend/plugins/body.php
+++ openml_OS/views/pages/frontend/plugins/body.php
@@ -127,7 +127,11 @@ password = YOUR_PASSWORD</pre></li>
           <div class="page-header">
             <h1 id="rapidminer">RapidMiner</h1>
           </div>
-	You can design OpenML workflows in RapidMiner to directly interact with OpenML. The RapidMiner plugin is currently <a href="https://github.com/openml/rapidminer"> under development</a>.
+	You can design OpenML workflows in RapidMiner to directly interact with OpenML. The RapidMiner plugin is currently <a href="https://github.com/openml/rapidminer"> under active development</a>.
+	   <br>
+            <a href="downloads/RM-OpenmlConnector-1.0.0-all.jar">
+              <button class="btn btn-large btn-primary" type="button">Download Alpha Release</button>
+            </a>
 	</div>
       </div> <!-- end col-md-9 -->
       </div> <!-- end col-md-10 -->
