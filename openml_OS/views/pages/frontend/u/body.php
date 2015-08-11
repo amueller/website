95a307ecfc3745d8d74ec69cbe5a41b79d794a7f
janvanrijn <janvanrijn@gmail.com>
2015-08-11 17:03:00 +0200

    use rpage

--- openml_OS/views/pages/frontend/u/body.php
+++ openml_OS/views/pages/frontend/u/body.php
@@ -2,13 +2,13 @@
   <div class="col-xs-12 col-md-10 col-md-offset-1" id="mainpanel">
 
     <div class="tab-content">
-      <div class="tab-pane  <?php if(isset($this->usubpage)) { echo 'active'; } ?>" id="data">
+      <div class="tab-pane  <?php if(isset($this->subpage)) { echo 'active'; } ?>" id="data">
         <?php
         subpage('data');
         ?>
       </div> <!-- end intro tab -->
 
-      <div class="tab-pane <?php if(!isset($this->usubpage) and false !== strpos($_SERVER['REQUEST_URI'],'/u/')) { echo 'active'; }?>" id="userdetail">
+      <div class="tab-pane <?php if(!isset($this->subpage) and false !== strpos($_SERVER['REQUEST_URI'],'/u/')) { echo 'active'; }?>" id="userdetail">
         <?php
         subpage('user');
         ?>
@@ -29,9 +29,9 @@
         <li class="panel guidechapter">
           <a data-toggle="collapse" data-parent="#accordeon"  data-target="#pagelist"><i class="fa fa-cubes fa-fw fa-lg"></i> <b>My data</b></a>
           <ul class="sidenav nav collapse in" id="pagelist">
-            <li><a href="<?php echo $this->baseurl.'/data';?>">Data sets</a></li>
-            <li><a href="<?php echo $this->baseurl.'/flows';?>">Flows</a></li>
-            <li><a href="<?php echo $this->baseurl.'/runs';?>">Runs</a></li>
+            <li><a href="<?php echo BASE_URL .'u/data/' . $this->user_id;?>">Data sets</a></li>
+            <li><a href="<?php echo BASE_URL .'u/flows/' . $this->user_id;?>">Flows</a></li>
+            <li><a href="<?php echo BASE_URL .'u/runs/' . $this->user_id;?>">Runs</a></li>
           </ul>
         </li>
       </ul>
