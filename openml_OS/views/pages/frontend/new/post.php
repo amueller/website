d74c388e1116ccdc66b759397de06cdef4d7d22e
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2015-08-26 21:39:23 +0200

    better upload message

--- openml_OS/views/pages/frontend/new/post.php
+++ openml_OS/views/pages/frontend/new/post.php
@@ -1,9 +1,9 @@
 <?php
 /* * * * *
  * This script transforms the input of the form into
- * a uniform format, as used by the model. 
+ * a uniform format, as used by the model.
  * It converts a dataset name into an id and filters
- * the datasets with the wrong target attribute. 
+ * the datasets with the wrong target attribute.
  * * * * */
 
 if($this->subpage == 'task') {
@@ -16,7 +16,7 @@ if($this->subpage == 'task') {
     if($this->input->post($i->name) == false ) { continue; }
     $inputs[$i->name] = $this->input->post($i->name);
   }
-  
+
   // special retrieve datasets and target feature
   $constraints = $this->Dataset->nameVersionConstraints( $this->input->post( 'source_data' ), 'd' );
   $target_feature = '`d`.`default_target_attribute`';
@@ -25,21 +25,21 @@ if($this->subpage == 'task') {
   } else {
     unset( $inputs['target_feature'] );
   }
-  
-  // TODO: remove mapping, add logic in DB instead of here. 
-  if( $ttid == 2 ) { // exception. 
+
+  // TODO: remove mapping, add logic in DB instead of here.
+  if( $ttid == 2 ) { // exception.
     $datatype = array( 'numeric' );
   }
-  
-  $sql = 
-    'SELECT `d`.`did` AS `source_data`, `f`.`name` AS `target_feature` ' . 
-    'FROM `dataset` `d`,`data_feature` `f` ' . 
+
+  $sql =
+    'SELECT `d`.`did` AS `source_data`, `f`.`name` AS `target_feature` ' .
+    'FROM `dataset` `d`,`data_feature` `f` ' .
     'WHERE `d`.`did` = `f`.`did` ' .
     'AND `f`.`name` = "' . $target_feature . '" ' .
     'AND `f`.`NumberOfMissingValues` = 0 ' .
-    'AND `f`.`data_type` IN ("' . implode( '","', $datatype ) . '") ' . 
+    'AND `f`.`data_type` IN ("' . implode( '","', $datatype ) . '") ' .
     'AND ' . $constraints . ' ';
-  
+
   // TODO: remove these mappings in a good way.
   if( $ttid == 5 ) {
     // clustering. no target feature
@@ -49,8 +49,8 @@ if($this->subpage == 'task') {
     // data mining challange. labeled data set and dataset with missing labels
     $constraints1 = $this->Dataset->nameVersionConstraints( $this->input->post( 'source_data' ), 'd1' );
     $constraints2 = $this->Dataset->nameVersionConstraints( $this->input->post( 'source_data_labeled' ), 'd2' );
-    
-    $sql = 
+
+    $sql =
       'SELECT `d1`.`did` AS source_data, `d2`.`did` AS `source_data_labeled`, `f1`.`name` AS `target_feature` '.
       'FROM `dataset` `d1`, `dataset` `d2`, `data_feature` `f1`, `data_feature` `f2` '.
       'WHERE `d1`.`did` = `f1`.`did` '.
@@ -58,8 +58,8 @@ if($this->subpage == 'task') {
       'AND `f1`.`name` = "' . $target_feature . '" ' .
       'AND `f2`.`name` = "' . $target_feature . '" ' .
       'AND `f2`.`NumberOfMissingValues` = 0 ' .
-      'AND `f1`.`data_type` IN ("' . implode( '","', $datatype ) . '") ' . 
-      'AND `f2`.`data_type` IN ("' . implode( '","', $datatype ) . '") ' . 
+      'AND `f1`.`data_type` IN ("' . implode( '","', $datatype ) . '") ' .
+      'AND `f2`.`data_type` IN ("' . implode( '","', $datatype ) . '") ' .
       'AND ' . $constraints1 . ' AND ' . $constraints2;
   }
   if( $ttid == 7 ) {
@@ -67,25 +67,25 @@ if($this->subpage == 'task') {
     $tfr = $this->input->post( 'target_feature_right' );
     $tfe = $this->input->post( 'target_feature_event' );
     $source_data = $this->input->post( 'source_data' );
-    
+
     if( ($tfl == false && $tfr == false) || $tfe == false || $source_data == false ) {
       // illegal request, return nothing
       $sql = 'SELECT * FROM dataset WHERE 0;';
     } else {
-      $sql = 
+      $sql =
         'SELECT `d`.`did` AS `source_data`, `fl`.`name` AS `target_feature_left`, ' .
           '`fr`.`name` AS `target_feature_right`, '.
           '`fe`.`name` AS `target_feature_event` ' .
         'FROM `data_feature` `fe`, `dataset` `d` ' .
         'LEFT JOIN `data_feature` `fl` ON `d`.`did` = `fl`.`did` AND `fl`.`name` = "' . $tfl . '" ' .
         'LEFT JOIN `data_feature` `fr` ON `d`.`did` = `fr`.`did` AND `fr`.`name` = "' . $tfr . '" ' .
-        'WHERE `d`.`did` = `fe`.`did` AND `fe`.`name` = "' . $tfe . '" ' . 
+        'WHERE `d`.`did` = `fe`.`did` AND `fe`.`name` = "' . $tfe . '" ' .
         'AND ' . $constraints . ' ';
     }
   }
-  
+
   $datasets = $this->Dataset->query( $sql . 'ORDER BY `source_data`;' );
-  
+
   // sanity check input
   $input_safe = true;
   // first sanitize custom testset
@@ -97,9 +97,9 @@ if($this->subpage == 'task') {
       $input_safe = false;
     }
   }
-  
+
   // TODO: Check input $this->input->post('cost_matrix')
-  
+
   $results = array(); // resulting task configurations
   $dids = array();    // used dataset ids
   $new_tasks = array();
@@ -116,7 +116,7 @@ if($this->subpage == 'task') {
       // against the rules
     } elseif( $input_safe ) {
       $new_tasks = $this->Task->create_batch( $ttid, $results );
-    } 
+    }
   }
   $inputs['source_data'] = $dids;
 
@@ -130,7 +130,7 @@ if($this->subpage == 'task') {
   }
 
   if( $new_tasks ) { $this->new_text = '* new'; }
-  
+
 } elseif($this->subpage == 'data') {
 
   $session_hash = $this->Api_session->createByUserId( $this->ion_auth->user()->row()->id );
@@ -162,8 +162,8 @@ if($this->subpage == 'task') {
       $this->response = 'Error '.$this->responsecode.': '.$xml->children('oml', true)->message;
       if($this->responsecode=='131') $this->response .= ' Please fill in all required (red) fields, upload a file or give a URL (not both), and avoid spaces in the dataset name.';
     } else if($xml->children('oml', true)->id){
-      $this->response = '<h2><i class="fa fa-thumbs-o-up"></i> Thanks!</h2>Data was uploaded successfully.<br>You can now <a href="d/'. $xml->children('oml', true)->id . '"> follow your dataset on OpenML</a>, track its impact and see all ensuing results.<br><br>You can also continue to add datasets below.';
-      sm($this->response);  
+      $this->response = '<h2><i class="fa fa-thumbs-o-up"></i> Thanks!</h2>Data was uploaded successfully (ID = '.$xml->children('oml', true)->id).')<br> You can now <b><a href="d/'. $xml->children('oml', true)->id . '"> follow your dataset on OpenML</a></b>, complete its description, track its impact, create OpenML tasks, and see all ensuing results.<br><br>You can also continue to add datasets below.';
+      sm($this->response);
       su('new/data');
     } else {
       print "Something went wrong. Server says:<br>";
