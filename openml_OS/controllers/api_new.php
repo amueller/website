e0098224ee543663c6214cad10ca638bf76a28c5
janvanrijn <janvanrijn@gmail.com>
2015-08-31 12:23:08 +0200

    added setups

--- openml_OS/controllers/api_new.php
+++ openml_OS/controllers/api_new.php
@@ -16,6 +16,7 @@ class Api_new extends CI_Controller {
     $this->load->model('api/v1/Api_tasktype');
     $this->load->model('api/v1/Api_flow');
     $this->load->model('api/v1/Api_run');
+    $this->load->model('api/v1/Api_setup');
 
     $this->load->model('Log');
     $this->load->model('Api_session');
