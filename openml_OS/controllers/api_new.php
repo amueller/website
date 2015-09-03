f939aae28807ffd872e98cd2f11b03dbc69f3685
janvanrijn <janvanrijn@gmail.com>
2015-09-03 19:30:09 +0200

    bugfix

--- openml_OS/controllers/api_new.php
+++ openml_OS/controllers/api_new.php
@@ -52,7 +52,7 @@ class Api_new extends CI_Controller {
     
     // some user authentication things.
     $this->provided_hash = $this->input->get_post('api_key') != false;
-    $this->provided_valid_hash = $this->Author->getWhere( 'session_hash = "' . $this->input->get_post('session_hash') . '"' ); // TODO: and add date?
+    $this->provided_valid_hash = $this->Author->getWhere( 'session_hash = "' . $this->provided_hash . '"' ); // TODO: and add date?
     $this->authenticated = $this->provided_valid_hash || $this->ion_auth->logged_in();
     $this->user_id = false;
     if($this->provided_valid_hash) {
