b47425120993e6526aa70bd6fc306bf8f6fdd4b2
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2016-01-21 01:51:48 +0100

    fixes run delete button

--- js/openml.js
+++ js/openml.js
@@ -81,11 +81,13 @@ Ps.initialize(container);
   // deleting items
   function deleteItem( type, id, name ) {
   console.log("DELETE http://" + location.host + "/api_new/v1/"+type+"/"+id)
+
   $.ajax({
     type: "DELETE",
     url: "http://" + location.host + "/api_new/v1/"+type+"/"+id,
     dataType: "xml"
   }).done( function( resultdata ) {
+      console.log(resultdata);
       console.log(resultdata.responseText);
       id_field = $(resultdata.responseText).find("oml\\:id, id");
       if( id_field.length ) {
