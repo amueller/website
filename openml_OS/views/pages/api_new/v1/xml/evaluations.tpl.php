623955ebed5550e82533df1b9ab20ce6f50cfee3
janvanrijn <janvanrijn@gmail.com>
2016-02-24 13:30:42 +1300

    bugfix

--- openml_OS/views/pages/api_new/v1/xml/evaluations.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/evaluations.tpl.php
@@ -6,8 +6,8 @@
     <oml:setup_id><?php echo $e->sid; ?></oml:setup_id>
     <oml:flow_id><?php echo $e->implementation_id; ?></oml:flow_id>
     <oml:function><?php echo $e->{'function'}; ?></oml:function>
-    <oml:value><?php echo $e->value; ?></oml:value>
-    <oml:array_data><?php echo $e->array_data; ?></oml:array_data>
+    <?php if($e->value != null): ?><oml:value><?php echo $e->value; ?></oml:value><?php endif; ?>
+    <?php if($e->array_data != null): ?><oml:array_data><?php echo $e->array_data; ?></oml:array_data><?php endif; ?>
   </oml:evaluation>
   <?php endforeach; ?>
 </oml:evaluations>
