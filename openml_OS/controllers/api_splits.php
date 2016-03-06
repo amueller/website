1e20185f1d109667e8f4b9bc4036cc76b1a59b72
janvanrijn <janvanrijn@gmail.com>
2016-03-07 10:44:01 +1300

    new split mechanism

--- openml_OS/controllers/api_splits.php
+++ openml_OS/controllers/api_splits.php
@@ -100,23 +100,10 @@ class Api_splits extends CI_Controller {
     }
     $values = $this->Task_inputs->getTaskValuesAssoc( $task_id );
     
-    $estimation_procedure = null;
-    if( array_key_exists( 'estimation_procedure', $values ) ) {
-      $estimation_procedure = $this->Estimation_procedure->getById( $values['estimation_procedure'] );
-    }
-    
-    if($estimation_procedure == false) {
-      die('estimation procedure not found');
-    }
-    
-    $dataset = $this->Dataset->getById( $values['source_data'] );
-    $epstr = $this->Estimation_procedure->toString( $estimation_procedure );
-    $target_feature = ( $task->ttid == 7 ) ? $values['target_feature_event'] : $values['target_feature'];
-    
     // TODO: very important. sanity check input
     $testset_str = array_key_exists('custom_testset', $values) && is_cs_natural_numbers($values['custom_testset']) ?  '-test "' . $values['custom_testset'] . '"' : '';
     
-    $command = 'java -jar '.$this->evaluation.' -f "generate_folds" -d "'.$dataset->url.'" -e "'.$epstr.'" -c "'.$target_feature.'" ' . $testset_str . $this->config; 
+    $command = 'java -jar '.$this->evaluation.' -f "generate_folds" -id ' . $task_id . ' ' . $this->config; 
     
     if (array_key_exists('custom_testset', $values)) {
       $command .= '-test "' . $values['custom_testset'] . '" ';
