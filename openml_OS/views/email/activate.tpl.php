1641ddbd8fe970c1ca7dff79d28ed40f633ba2d4
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2016-03-09 11:07:57 +0100

    add account activation link in clear text, for email clients that block html

--- openml_OS/views/email/activate.tpl.php
+++ openml_OS/views/email/activate.tpl.php
@@ -4,6 +4,8 @@
 	<p>Before you go out and play on OpenML, please verify your email address.</p>
 	<p>Click this link to <?php echo anchor('frontend/page/profile_activate/id/'. $id .'/code/'. $activation, 'Activate Your Account');?>.</p>
 
+	<p>If you are unable to click the link, please copy/paste this url: http://www.openml.org/profile_activate/id/<?php echo $id .'/code/'. $activation; ?></p>
+
 	<p>If you did not create an OpenML account using this address, please contact us at openmachinelearning@gmail.com.</p>
 </body>
 </html>
