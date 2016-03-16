1de19d45eb34b1ae14810f6e3db5a7f253989140
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2016-03-16 15:18:54 +0100

    fixed tagging for datasets

--- openml_OS/views/pages/frontend/d/post.php
+++ openml_OS/views/pages/frontend/d/post.php
@@ -7,9 +7,7 @@ if(isset($_POST["newtags"]) and !empty($_POST["newtags"])){
                      'tag' => $_POST["newtags"]);
   $url = 'http://www.openml.org/api/v1/data/tag';
   $api_response = $this->curlhandler->post_helper($url,$post_data);
-  print_r($post_data);
-  print_r($api_response);
-  //redirect('d/'.$this->id);
+  redirect('d/'.$this->id);
 }
 elseif(isset($_POST["deletetag"]) and !empty($_POST["deletetag"])){
   $post_data = array('api_key' => $this->ion_auth->user()->row()->session_hash,
