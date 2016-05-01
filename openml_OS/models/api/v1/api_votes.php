4ae3a69337aa6bb5c19251ac2ad1aa6c69f11186
Kris <krisvantienhoven@gmail.com>
2016-05-01 02:08:37 +0200

    less errors, more empty arrays

--- openml_OS/models/api/v1/api_votes.php
+++ openml_OS/models/api/v1/api_votes.php
@@ -53,8 +53,9 @@ class Api_votes extends Api_model {
     private function up_list() {
         $likes_res = $this->Like->get();
         if (is_array($likes_res) == false) {
-            $this->returnError(701, $this->version);
-            return;
+            //$this->returnError(701, $this->version);
+            //return;
+            $likes_res = [];
         }
 
         $this->xmlContents('likes', $this->version, array('likes' => $likes_res));
@@ -63,8 +64,9 @@ class Api_votes extends Api_model {
     private function down_list() {
         $dvotes_res = $this->Downvote->get();
         if (is_array($dvotes_res) == false) {
-            $this->returnError(701, $this->version);
-            return;
+            //$this->returnError(701, $this->version);
+            //return;
+            $dvotes_res = [];
         }
 
         $this->xmlContents('downvotes', $this->version, array('downvotes' => $dvotes_res));
@@ -73,8 +75,9 @@ class Api_votes extends Api_model {
     private function up_of_user($user_id) {
         $likes_res = $this->Like->getLikesByUser($user_id);
         if (is_array($likes_res) == false) {
-            $this->returnError(702, $this->version);
-            return;
+            //$this->returnError(702, $this->version);
+            //return;
+            $likes_res = [];
         }
         $this->xmlContents('likes', $this->version, array('likes' => $likes_res));
     }
@@ -82,8 +85,9 @@ class Api_votes extends Api_model {
     private function down_of_user($user_id) {
         $dvotes_res = $this->Downvote->getDownvotesByUser($user_id);
         if (is_array($dvotes_res) == false) {
-            $this->returnError(702, $this->version);
-            return;
+            //$this->returnError(702, $this->version);
+            //return;
+            $dvotes_res = [];
         }
         $this->xmlContents('downvotes', $this->version, array('downvotes' => $dvotes_res));
     }
@@ -91,8 +95,9 @@ class Api_votes extends Api_model {
     private function up_of_piece($knowledge_type, $knowledge_id){
         $likes_res = $this->Like->getLikesByKnowledgePiece($knowledge_type, $knowledge_id);
         if (is_array($likes_res) == false) {
-            $this->returnError(702, $this->version);
-            return;
+            //$this->returnError(702, $this->version);
+            //return;
+            $likes_res = [];
         }
         $this->xmlContents('likes', $this->version, array('likes' => $likes_res));        
     }
@@ -100,8 +105,9 @@ class Api_votes extends Api_model {
     private function down_of_piece($knowledge_type, $knowledge_id){
         $dvotes_res = $this->Downvote->getDownvotesByKnowledgePiece($knowledge_type, $knowledge_id);
         if (is_array($dvotes_res) == false) { //fix dit
-            $this->returnError(702, $this->version);
-            return;
+            //$this->returnError(702, $this->version);
+            //return;
+            $dvotes_res = [];
         }
         $this->xmlContents('downvotes', $this->version, array('downvotes' => $dvotes_res));      
     }
@@ -215,6 +221,7 @@ class Api_votes extends Api_model {
             $dvote = $this->Downvote->getByIds($this->user_id, $knowledge_type, $knowledge_id)[0];
             
             if ($dvote == false) {
+                //no issue found, but potentially an agreement
                 $dvote = $this->Downvote->getByIds($this->user_id, $knowledge_type, $knowledge_id,0)[0];
                 if($dvote == false){
                     $this->returnError(703, $this->version);
