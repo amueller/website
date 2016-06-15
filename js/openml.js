36f2d75e1d27c80187e77e735726d96e974fbcca
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2016-06-15 17:46:26 +0200

    bugfixing JS

--- js/openml.js
+++ js/openml.js
@@ -113,7 +113,6 @@ Ps.initialize(container);
           swal("Error", "Your login has expired. Log in and try again.", "error");
         else
           swal("Error " + code_field, message_field, "error");
-        location.reload();
     });
   }
 
