c220de8239d19708778efe8f34e0b1e4f5732b5a
janvanrijn <janvanrijn@gmail.com>
2016-06-27 10:26:30 +0200

    changed of parameters

--- openml_OS/views/pages/api_new/v1/xml/setup-parameters.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/setup-parameters.tpl.php
@@ -1,13 +1,11 @@
 <oml:setup_parameters xmlns:oml="http://openml.org/openml">
-	<oml:parameters>
-		<?php foreach( $parameters as $p ): ?>
-			<oml:parameter>
-				<oml:full_name><?php echo $p->fullName; ?></oml:full_name>
-				<oml:parameter_name><?php echo $p->name; ?></oml:parameter_name>
-				<oml:data_type><?php echo $p->dataType; ?></oml:data_type>
-				<oml:default_value><?php echo $p->defaultValue; ?></oml:default_value>
-				<oml:value><?php echo $p->value; ?></oml:value>
-			</oml:parameter>
-		<?php endforeach; ?>
-	</oml:parameters>
+<?php foreach( $parameters as $p ): ?>
+	<oml:parameter>
+		<oml:full_name><?php echo $p->fullName; ?></oml:full_name>
+		<oml:parameter_name><?php echo $p->name; ?></oml:parameter_name>
+		<oml:data_type><?php echo $p->dataType; ?></oml:data_type>
+		<oml:default_value><?php echo $p->defaultValue; ?></oml:default_value>
+		<oml:value><?php echo $p->value; ?></oml:value>
+	</oml:parameter>
+<?php endforeach; ?>
 </oml:setup_parameters>
