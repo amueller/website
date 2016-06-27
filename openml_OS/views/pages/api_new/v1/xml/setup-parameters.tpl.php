27fd7dfe4d7ddc7c01c0ae71f457f49566b8f62f
janvanrijn <janvanrijn@gmail.com>
2016-06-27 09:10:15 +0200

    bugfix params

--- openml_OS/views/pages/api_new/v1/xml/setup-parameters.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/setup-parameters.tpl.php
@@ -2,13 +2,10 @@
 	<oml:parameters>
 		<?php foreach( $parameters as $p ): ?>
 			<oml:parameter>
-				<oml:full_name><?php echo $p->input; ?></oml:full_name>
+				<oml:full_name><?php echo $p->fullName; ?></oml:full_name>
 				<oml:parameter_name><?php echo $p->name; ?></oml:parameter_name>
-				<oml:general_name><?php echo $p->generalName; ?></oml:general_name>
 				<oml:data_type><?php echo $p->dataType; ?></oml:data_type>
 				<oml:default_value><?php echo $p->defaultValue; ?></oml:default_value>
-				<oml:min><?php echo $p->min; ?></oml:min>
-				<oml:max><?php echo $p->max; ?></oml:max>
 				<oml:value><?php echo $p->value; ?></oml:value>
 			</oml:parameter>
 		<?php endforeach; ?>
