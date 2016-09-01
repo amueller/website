a1fc8dcc3d0124fa4b26eca4fe9dc915e5c09d7c
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2016-09-01 13:26:28 +0200

    Added dataset version to API list function

--- openml_OS/views/pages/api_new/v1/xml/data.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/data.tpl.php
@@ -3,6 +3,7 @@
   <oml:dataset>
     <oml:did><?php echo $data->did; ?></oml:did>
     <oml:name><?php echo $data->name; ?></oml:name>
+    <oml:version><?php echo $data->version; ?></oml:version>
     <oml:status><?php echo $data->status; ?></oml:status>
     <oml:format><?php echo $data->format; ?></oml:format>
     <?php foreach( $data->qualities as $quality => $value ): ?>
