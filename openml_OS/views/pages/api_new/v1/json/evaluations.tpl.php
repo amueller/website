e70c5f4a92a9392c139d02c0b771a06c5f48e79b
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2016-09-06 11:46:30 +0200

    updated json templates

--- openml_OS/views/pages/api_new/v1/json/evaluations.tpl.php
+++ openml_OS/views/pages/api_new/v1/json/evaluations.tpl.php
@@ -7,9 +7,9 @@
    "task_id":<?php echo $e->task_id; ?>,
    "setup_id":<?php echo $e->sid; ?>,
    "flow_id":<?php echo $e->implementation_id; ?>,
-   "function":"<?php echo $e->{'function'}; ?>",
-   <?php if($e->value != null): ?>"value":<?php echo $e->value; ?>,<?php endif; ?>
-   <?php if($e->array_data != null): ?>"array_data":<?php echo $e->array_data; ?><?php endif; ?>
+   "function":"<?php echo $e->{'function'}; ?>"
+   <?php if($e->value != null): ?>",value":<?php echo $e->value; ?>,<?php endif; ?>
+   <?php if($e->array_data != null): ?>",array_data":<?php echo $e->array_data; ?><?php endif; ?>
   }
   <?php endforeach; ?>
   ]}
