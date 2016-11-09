ccbf9c0f5e92268b3e5c06c8a5fa19e03cef398d
Jan van Rijn <janvanrijn@gmail.com>
2016-11-09 15:09:30 +0100

    more error control operators for optional parameters

--- openml_OS/models/api/v1/Api_job.php
+++ openml_OS/models/api/v1/Api_job.php
@@ -27,11 +27,11 @@ class Api_job extends Api_model {
   private function job_request() {
     $workbench = safe($this->input->get_post('workbench'));
     $task_type_id = safe($this->input->get_post('task_type_id'));
-    $task_tag = safe($this->input->get_post('task_tag'));
-    $setup_tag = safe($this->input->get_post('setup_tag'));
-    $setup_id = safe($this->input->get_post('setup_id'));
+    $task_tag = safe(@$this->input->get_post('task_tag'));
+    $setup_tag = safe(@$this->input->get_post('setup_tag'));
+    $setup_id = safe(@$this->input->get_post('setup_id'));
 
-    if( $workbench == false || $task_type_id == false ) {
+    if($workbench == false || $task_type_id == false) {
       $this->returnError(340, $this->version);
       return;
     }
