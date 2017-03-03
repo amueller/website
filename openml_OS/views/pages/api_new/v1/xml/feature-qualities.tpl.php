dec76a8d66fe2d736c3f0130cf66d5f87c8f25e2
William Raynaut <william.raynaut@gmail.com>
2017-03-03 13:09:54 +0100

    feature-qualities.tpl.php recommit

--- openml_OS/views/pages/api_new/v1/xml/feature-qualities.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/feature-qualities.tpl.php
@@ -1,6 +1,6 @@
 <oml:feature_qualities xmlns:oml="http://openml.org/openml">
   <?php foreach( $qualities as $quality ): ?>
-
+  
   <oml:quality>
     <oml:name><?php echo $quality->quality; ?></oml:name>
     <oml:value><?php echo $quality->value; ?></oml:value>
