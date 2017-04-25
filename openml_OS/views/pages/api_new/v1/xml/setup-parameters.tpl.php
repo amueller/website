2c8676ec7d8e73c5832ddcf16b1fe4cfb6294da7
Jan van Rijn <janvanrijn@gmail.com>
2017-04-25 16:43:53 +0200

    added information for setup parameters

--- openml_OS/views/pages/api_new/v1/xml/setup-parameters.tpl.php
+++ openml_OS/views/pages/api_new/v1/xml/setup-parameters.tpl.php
@@ -2,6 +2,8 @@
   <oml:flow_id><?php echo $setup->implementation_id; ?></oml:flow_id>
 <?php foreach($parameters as $p): ?>
 	<oml:parameter>
+		<oml:id><?php echo htmlspecialchars($p->id); ?></oml:id>
+		<oml:flow_id><?php echo htmlspecialchars($p->implementation_id); ?></oml:flow_id>
 		<oml:full_name><?php echo htmlspecialchars($p->fullName); ?></oml:full_name>
 		<oml:parameter_name><?php echo htmlspecialchars($p->name); ?></oml:parameter_name>
 		<oml:data_type><?php echo htmlspecialchars($p->dataType); ?></oml:data_type>
