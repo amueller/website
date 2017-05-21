0c9b724e0160c8cf9952a4933e953a5167d6d20c
Jan van Rijn <janvanrijn@gmail.com>
2017-05-21 15:32:55 +0200

    bugfix

--- openml_OS/models/api/v1/Api_evaluation.php
+++ openml_OS/models/api/v1/Api_evaluation.php
@@ -24,10 +24,10 @@ class Api_evaluation extends Api_model {
     $order_values = array('random', 'reverse', 'normal');
     if (count($segments) >= 3 && $segments[0] == 'request' && is_numeric($segments[1]) && in_array($segments[2], $order_values)) {
       if (count($segments) == 3) {
-        $this->evaluation_list($segments[1], $segments[2], false);
+        $this->evaluation_request($segments[1], $segments[2], false);
         return;
       } elseif (count($segments) == 4 && is_numeric($segments[4])) {
-        $this->evaluation_list($segments[1], $segments[2], $segments[3]);
+        $this->evaluation_request($segments[1], $segments[2], $segments[3]);
         return;
       }
     }
