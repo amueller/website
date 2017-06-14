c0e52264e6e22ea65c070c97bd35da917e9dd1d0
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2017-06-14 12:54:08 +0200

    auto-init indexes

--- openml_OS/libraries/ElasticSearch.php
+++ openml_OS/libraries/ElasticSearch.php
@@ -357,7 +357,7 @@ class ElasticSearch {
         //bootstrap
         $indexParams['index'] = 'openml';
         $indexParams['type'] = $type;
-        if(! $this->$client->indices()->exists($indexParams))
+        if(! $this->$client->indices()->getMapping($indexParams))
           echo $this->initialize_index($type);
 
 	      $method_name = 'index_' . $type;
@@ -376,7 +376,7 @@ class ElasticSearch {
         //bootstrap
         $indexParams['index'] = 'openml';
         $indexParams['type'] = $type;
-        if(! $this->client->indices()->exists($indexParams))
+        if(! $this->client->indices()->getMapping($indexParams))
           echo $this->initialize_index($type);
 
         $method_name = 'index_' . $type;
