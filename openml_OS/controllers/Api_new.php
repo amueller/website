dc1c0754575f26ae27481cb19d6c669a8df9efee
Jan van Rijn <janvanrijn@gmail.com>
2017-07-06 14:12:42 +0200

    changed back run upload directory

--- openml_OS/controllers/Api_new.php
+++ openml_OS/controllers/Api_new.php
@@ -42,7 +42,7 @@ class Api_new extends CI_Controller {
     $this->data_folders = array(
       'dataset'        => 'dataset/api/',
       'implementation' => 'implementation/',
-      'run'            => 'run2/',
+      'run'            => 'run/',
       'misc'           => 'misc/'
     );
 
