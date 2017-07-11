77a72dc85a3874098630837eaa1c62163956e944
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2017-07-11 23:26:20 +0100

    fixes bug where non-active datasets do not show up in a user's dataset list

--- openml_OS/views/pages/frontend/search/pre.php
+++ openml_OS/views/pages/frontend/search/pre.php
@@ -195,7 +195,10 @@ foreach($this->filters as $k => $v){
 		if(count($parts) == 2)
 			$jsonfilters[] = '{ "range" : { "'.$k.'" : { "gte" : '.$parts[0].', "lte" : '.$parts[1].' } } }';
 		}
-	elseif($k == 'type' or $k == 'measure_type' or $k == 'status' or $k == 'evaluation_measures')
+  elseif($k == 'status'){
+    if($v != 'all')
+      $jsonfilters[] = '{ "term" : { "'.$k.'" : "'.$v.'"} }';}
+	elseif($k == 'type' or $k == 'measure_type' or $k == 'evaluation_measures')
     $jsonfilters[] = '{ "term" : { "'.$k.'" : "'.$v.'"} }';
   elseif($k == 'tags.tag')
     $jsonfilters[] = '{ "nested": { "path": "tags", "query": { "term": { "tags.tag": "'.strtolower($v).'" } } } }';
