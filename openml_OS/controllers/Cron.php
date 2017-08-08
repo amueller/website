a680b25f5b63281a38ae8e286216a4aff508d3e6
janvanrijn@gmail.com <janvanrijn@gmail.com>
2017-08-08 12:25:59 +0200

    updated cron create meta dataset

--- openml_OS/controllers/Cron.php
+++ openml_OS/controllers/Cron.php
@@ -107,47 +107,36 @@ class Cron extends CI_Controller {
     }
   }
 
-  function create_meta_dataset( $id = false ) {
-    if( $id == false ) {
-      $meta_dataset = $this->Meta_dataset->getWhere( 'processed IS NULL' );
+  function create_meta_dataset($id = false) {
+    if ($id == false) {
+      $meta_dataset = $this->Meta_dataset->getWhere('processed IS NULL');
       echo 'No id specified, requesting first dataset in queue.' . "\n";
     } else {
-      $meta_dataset = $this->Meta_dataset->getWhere( 'id = "' . $id . '"' );
+      $meta_dataset = $this->Meta_dataset->getWhere('id = "' . $id . '"');
       echo 'Requesting dataset with id ' . $id . ".\n";
     }
 
-    if( $meta_dataset ) {
+    if ($meta_dataset) {
       $meta_dataset = $meta_dataset[0];
       echo 'Processing meta-dataset with id ' . $meta_dataset->id . ".\n";
-      $this->Meta_dataset->update( $meta_dataset->id, array( 'processed' => now() ) );
-      $dataset_constr = ( $meta_dataset->datasets ) ? 'AND d.did IN (' . $meta_dataset->datasets . ') ' : '';
-      $task_constr = ( $meta_dataset->tasks ) ? 'AND t.task_id IN (' . $meta_dataset->tasks . ') ' : '';
-      $flow_constr = ( $meta_dataset->flows ) ? 'AND i.id IN (' . $meta_dataset->flows . ') ' : '';
-      $setup_constr = ( $meta_dataset->setups ) ? 'AND s.sid IN (' . $meta_dataset->setups . ') ' : '';
-      $function_constr = ( $meta_dataset->functions ) ? 'AND e.function IN (' . $meta_dataset->functions . ') ' : '';
+      $this->Meta_dataset->update( $meta_dataset->id, array('processed' => now()));
+      $dataset_constr = ($meta_dataset->datasets) ? 'AND d.did IN (' . $meta_dataset->datasets . ') ' : '';
+      $task_constr = ($meta_dataset->tasks) ? 'AND t.task_id IN (' . $meta_dataset->tasks . ') ' : '';
+      $flow_constr = ($meta_dataset->flows) ? 'AND i.id IN (' . $meta_dataset->flows . ') ' : '';
+      $setup_constr = ($meta_dataset->setups) ? 'AND s.sid IN (' . $meta_dataset->setups . ') ' : '';
+      $function_constr = ($meta_dataset->functions) ? 'AND m.name IN (' . $meta_dataset->functions . ') ' : '';
 
       $quality_colum = 'data_quality';
       $evaluation_column = 'evaluation';
-      $evaluation_keys = array( 'function' => 'e.function' );
+      $evaluation_keys = array('m.name');
       $quality_keys = array();
 
-      if( $meta_dataset->task_type == 3 ) {
-        $evaluation_keys = array(
-          'repeat' => 'e.repeat',
-          'fold' => 'e.fold',
-          'sample' => 'e.sample',
-          'sample_size' => 'e.sample_size',
-          'function' => 'e.function'
-        );
-
+      if ($meta_dataset->task_type == 3) {
+        $evaluation_keys = array('e.repeat', 'e.fold', 'e.sample', 'e.sample_size', 'm.name');
         $evaluation_column = 'evaluation_sample';
       }
-      if( $meta_dataset->task_type == 4 ) {
-        $evaluation_keys = array(
-          'interval_start' => 'e.interval_start',
-          'interval_end'   => 'e.interval_end',
-          'function' => 'e.function'
-        );
+      if ($meta_dataset->task_type == 4) {
+        $evaluation_keys = array('e.interval_start', 'e.interval_end', 'm.name');
         $quality_keys = array(
           'interval_start' => 'q.interval_start',
           'interval_end'   => 'q.interval_end'
@@ -157,8 +146,8 @@ class Cron extends CI_Controller {
 
       }
 
-      if ( create_dir(DATA_PATH . $this->dir_suffix) == false ) {
-        $this->_error_meta_dataset( $meta_dataset->id, 'Failed to create data directory. ', $meta_dataset->user_id );
+      if (create_dir(DATA_PATH . $this->dir_suffix) == false) {
+        $this->_error_meta_dataset($meta_dataset->id, 'Failed to create data directory. ', $meta_dataset->user_id);
         return;
       }
 
@@ -167,8 +156,8 @@ class Cron extends CI_Controller {
       if($meta_dataset->type == 'qualities') {
         $quality_keys_string = '';
         if( $quality_keys ) {
-          $quality_keys_string = implode( ', ', $quality_keys ) . ',';
-          $quality_keys_key_string = '"' . implode( '", "', array_keys( $quality_keys ) ) . '",';
+          $quality_keys_string = implode(', ', $quality_keys) . ',';
+          $quality_keys_key_string = '"' . implode('", "', array_keys($quality_keys)) . '",';
         }
         $sql =
           'SELECT "data_id", "task_id", "name", "quality", ' . $quality_keys_key_string . ' "value" ' .
@@ -177,7 +166,7 @@ class Cron extends CI_Controller {
           'FROM dataset d, '.$quality_colum.' q, task t, task_inputs i ' .
           'WHERE t.task_id = i.task_id ' .
           'AND i.input = "source_data" ' .
-          'AND i.value = q.data ' .
+          'AND i.value = q.data AND q.evaluation_engine_id = 1 ' .
           'AND d.did = q.data ' .
           'AND t.ttid = "' . $meta_dataset->task_type . '" ' .
           $dataset_constr . $task_constr .
@@ -186,31 +175,32 @@ class Cron extends CI_Controller {
           'ENCLOSED BY "\"" ' .
           'LINES TERMINATED BY "\n" ' .
           ';';
-      } elseif($meta_dataset->type == 'evaluations') {
+      } elseif ($meta_dataset->type == 'evaluations') {
         $sql =
-          'SELECT "run_id", "setup_id", "task_id", "' . implode( '", "', array_keys( $evaluation_keys ) ) . '" ' .
+          'SELECT "run_id", "setup_id", "task_id", "' . implode('", "', $evaluation_keys) . '" ' .
           ', "value", "task_name", "flow_name" ' .
           'UNION ALL ' .
-          'SELECT r.rid AS run_id, s.sid AS setup_id, t.task_id AS task_id, '.
-          implode( ', ', $evaluation_keys ) . ', e.value '.
-          ', CONCAT("Task_", t.task_id, "_", d.name), i.fullName '.
+          'SELECT r.rid AS run_id, s.sid AS setup_id, t.task_id AS task_id, ' .
+          implode(', ', $evaluation_keys) . ', e.value ' .
+          ', CONCAT("Task_", t.task_id, "_", d.name), i.fullName ' .
 //          ',s.setup_string ' .
-//          ',CONCAT(i.fullName, " on ", d.name) as textual '.
-          'FROM run r, task t, task_inputs v, dataset d, algorithm_setup s, implementation i, '. $evaluation_column .' e '.
-          'WHERE r.task_id = t.task_id AND v.task_id = t.task_id  '.
-          'AND v.input = "source_data" AND v.value = d.did '.
-          'AND r.setup = s.sid AND s.implementation_id = i.id '.
-          'AND e.source = r.rid '.
+//          ',CONCAT(i.fullName, " on ", d.name) as textual ' .
+          'FROM run r, task t, task_inputs v, dataset d, algorithm_setup s, implementation i, ' . $evaluation_column . ' e, math_function m ' .
+          'WHERE r.task_id = t.task_id AND v.task_id = t.task_id  ' .
+          'AND v.input = "source_data" AND v.value = d.did ' .
+          'AND r.setup = s.sid AND s.implementation_id = i.id ' .
+          'AND e.source = r.rid ' .
+          'AND e.function_id = m.id AND e.evaluation_engine_id = 1 ' .
           'AND t.ttid = "' . $meta_dataset->task_type . '"' .
           $dataset_constr . $task_constr . $flow_constr . $setup_constr . $function_constr .
            /* the GROUP BY line makes stuff slower, we might want to comment it out. */
-          'GROUP BY r.setup, r.task_id, ' . implode( ',', $evaluation_keys ) . ' ' .
-          'INTO OUTFILE "'. $tmp_path .'" ' .
+          'GROUP BY r.setup, r.task_id, ' . implode(',', $evaluation_keys) . ' ' .
+          'INTO OUTFILE "' . $tmp_path .'" ' .
           'FIELDS TERMINATED BY "," ' .
           'ENCLOSED BY "\"" ' .
           'LINES TERMINATED BY "\n" ' .
           ';';
-      } elseif($meta_dataset->type == 'inputs') {
+      } elseif ($meta_dataset->type == 'inputs') {
         $sql =
           'SELECT is.setup, i.fullname AS flowname, ip.name, is.value ' .
           'FROM `input_setting` `is` , input `ip`, algorithm_setup s, implementation i ' .
@@ -229,32 +219,32 @@ class Cron extends CI_Controller {
         return;
       }
 
-      $this->Dataset->query( $sql );
-      $success = file_exists( $tmp_path );
+      $this->Dataset->query($sql);
+      $success = file_exists($tmp_path);
 
-      if( $success == false ) {
+      if($success == false) {
         $error = 'MySQL Error #' . $this->Dataset->mysqlErrorNo() . ': ' . $this->Dataset->mysqlErrorMessage();
-        $this->_error_meta_dataset( $meta_dataset->id, $error, $meta_dataset->user_id );
+        $this->_error_meta_dataset($meta_dataset->id, $error, $meta_dataset->user_id);
         return;
       }
 
-      $filename = getAvailableName( DATA_PATH . $this->dir_suffix, 'meta_dataset.csv' );
+      $filename = getAvailableName(DATA_PATH . $this->dir_suffix, 'meta_dataset.csv');
       $filepath = DATA_PATH . $this->dir_suffix . $filename;
-      $success = rename( $tmp_path, $filepath );
+      $success = rename($tmp_path, $filepath);
 
-      if( $success == false ) {
-        $this->_error_meta_dataset( $meta_dataset->id, 'Failed to move csv to data directory. Filename: ' . $filename, $meta_dataset->user_id );
+      if($success == false) {
+        $this->_error_meta_dataset($meta_dataset->id, 'Failed to move csv to data directory. Filename: ' . $filename, $meta_dataset->user_id);
         return;
       }
 
-      $file_id = $this->File->register_created_file( $this->dir_suffix, $filename, $meta_dataset->user_id, 'dataset', 'text/csv', 'private' );
+      $file_id = $this->File->register_created_file($this->dir_suffix, $filename, $meta_dataset->user_id, 'dataset', 'text/csv', 'private');
 
-      $this->Meta_dataset->update( $meta_dataset->id, array( 'file_id' => $file_id ) );
+      $this->Meta_dataset->update($meta_dataset->id, array( 'file_id' => $file_id));
 
-      $user = $this->ion_auth->user( $meta_dataset->user_id )->row();
-      $this->email->to( $user->email );
+      $user = $this->ion_auth->user($meta_dataset->user_id)->row();
+      $this->email->to($user->email);
       $this->email->subject('OpenML Meta Dataset');
-      $this->email->message("This is an automatically generated email. The your requested meta-dataset was created successfully and can be downloaded from " . BASE_URL );
+      $this->email->message("This is an automatically generated email. The your requested meta-dataset was created successfully and can be downloaded from " . BASE_URL);
       $this->email->send();
     } else {
       echo 'No meta-dataset to process. '."\n";
