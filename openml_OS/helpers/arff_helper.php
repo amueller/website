b1f1a049b50a16cfafa18c48f9783c814b2f3f18
janvanrijn@gmail.com <janvanrijn@gmail.com>
2017-10-10 17:19:52 +0200

    update contributed by Tobias Glasmachers

--- openml_OS/helpers/arff_helper.php
+++ openml_OS/helpers/arff_helper.php
@@ -1,5 +1,4 @@
 <?php
-
 // This function extracts a valid ARFF string (or token) from the given line,
 // starting at the given position. It returns the position of the next
 // delimiter or the length of the line if none of the delimiters is found.
@@ -44,15 +43,15 @@ function ARFFparseString($line, $pos, &$token, $delimiters)
 	while ($end < $len and $line[$end] == ' ') $end++;
 	return $end;
 }
-
-// This function checks whether the content from $filehandle appears to be a 
+// This function checks whether the content from $filepath appears to be a 
 // valid ARFF file. It checks the header section and up to $numLines data lines,
 // in dense or sparse format. In case of an ARFF format violation it returns an
 // error message string, otherwise it returns TRUE.
 function ARFFcheck($filepath, $numLines = 100)
 {
 	// read header section
-	$nominalValue = [];
+	$featureType = [];
+	$numberOfNonNumericFeatures = 0;
 	$filehandle = fopen($filepath, "r");
 	$line = '';
 	$lineNumber = -1;
@@ -70,8 +69,8 @@ function ARFFcheck($filepath, $numLines = 100)
 			if ($type == "@data")
 			{
 				if ($pos != strlen($line)) {
-				  fclose($filehandle);
-				  return "trailing content in @data line (l.".$lineNumber.")";
+					fclose($filehandle);
+					return "trailing content in @data line (l.".$lineNumber.")";
 				}
 				break;
 			}
@@ -79,20 +78,20 @@ function ARFFcheck($filepath, $numLines = 100)
 			{
 				$pos = ARFFparseString($line, $pos, $s, " ");
 				if (is_string($pos)) {
-				  fclose($filehandle);
-				  return $pos . " (l.".$lineNumber.")";
+					fclose($filehandle);
+					return $pos . " (l.".$lineNumber.")";
 				}
 				if ($pos != strlen($line)) {
-				  fclose($filehandle);
-				  return "trailing content in @relation line (l.".$lineNumber.")";
+					fclose($filehandle);
+					return "trailing content in @relation line (l.".$lineNumber.")";
 				}
 			}
 			else if ($type == "@attribute")
 			{
 				$pos = ARFFparseString($line, $pos, $s, " ");
 				if (is_string($pos)) {
-				  fclose($filehandle);
-				  return $pos . " (l.".$lineNumber.")";
+					fclose($filehandle);
+					return $pos . " (l.".$lineNumber.")";
 				}
 				
 				if ($line[$pos] == '{')
@@ -103,47 +102,48 @@ function ARFFcheck($filepath, $numLines = 100)
 					{
 						$pos = ARFFparseString($line, $pos, $s, ",}");
 						if (is_string($pos)) {
-				      fclose($filehandle);
-						  return $pos . " (l.".$lineNumber.")";
+							fclose($filehandle);
+							return $pos . " (l.".$lineNumber.")";
 						}
 						array_push($values, $s);
 						if ($line[$pos] == ',') $pos++;
 						else if ($line[$pos] == '}') { $pos++; break; }
 						else {
-				      fclose($filehandle);
-						  return "format error in nominal attribute declaration" . " (l.".$lineNumber.")";
+							fclose($filehandle);
+							return "format error in nominal attribute declaration" . " (l.".$lineNumber.")";
 						}
 					}
-					array_push($nominalValue, $values);
+					array_push($featureType, $values);
+					$numberOfNonNumericFeatures++;
 				}
 				else
 				{
 					$pos = ARFFparseString($line, $pos, $s, "");
 					if (is_string($pos)) {
-				    fclose($filehandle);
-					  return $pos . " (l.".$lineNumber.")";
+						fclose($filehandle);
+						return $pos . " (l.".$lineNumber.")";
 					}
 					$s = strtolower($s);
-					if ($s != "binary" and $s != "numeric" and $s != "integer" and $s != "real" and $s != "string" and $s != "date")
+					if ($s == "binary" or $s == "integer" or $s == "real") $s = "numeric";   // map nonofficial types to official numeric type
+					if ($s != "numeric" and $s != "string" and $s != "date")
 					{ 
-				    fclose($filehandle);
+						fclose($filehandle);
 						return "unsupported attribute type: " . $s . " (l.".$lineNumber.")";
 					}
-					array_push($nominalValue, []);
+					array_push($featureType, s);
 				}
 			} else {
-			  fclose($filehandle);
-			  return "unsupported header field: " . $type . " (l.".$lineNumber.")";;
+				fclose($filehandle);
+				return "unsupported header field: " . $type . " (l.".$lineNumber.")";
 			}
 		}	else {
 			fclose($filehandle);
-		  return "invalid line in ARFF header: " . $line . " (l.".$lineNumber.")";;
+			return "invalid line in ARFF header: " . $line . " (l.".$lineNumber.")";
 		}
 		$line = strtok("\n");
 	}
-	$p = count($nominalValue);   // number of features
-
-	// read up to 10 lines of data
+	$p = count($featureType);   // number of features
+	// read up to $numLines lines of data
 	$counter = 0;
 	$line = strtok("\n");
 	while ($line !== FALSE)
@@ -158,11 +158,15 @@ function ARFFcheck($filepath, $numLines = 100)
 			if ($line[0] == '{')
 			{
 				// sparse data
+				// 1. check that all specified features have valid values
+				// 2. check that all nominal features are specified explicitly
+				$nonnum = 0;
 				$pos = 1;
 				while ($pos < $len and $line[$pos] == ' ') $pos++;
 				if ($pos < $len and $line[$pos] == '}')
 				{
 					// empty feature set is syntactically correct
+					// as long as all features are numeric
 				}
 				else
 				{
@@ -170,30 +174,64 @@ function ARFFcheck($filepath, $numLines = 100)
 					{
 						$pos = ARFFparseString($line, $pos, $s, " ");
 						if (is_string($pos)) {
-						  fclose($filehandle);
-						  return $pos . " (l.".$lineNumber.")";;
+							fclose($filehandle);
+							return $pos . " (l.".$lineNumber.")";
 						}
 						$i = intval($s);
+						if ($i < 0 or $i >= $p)
+						{
+							fclose($filehandle);
+							return "invalid feature index: " . $i . " (l.".$lineNumber.")";
+						}
 						$pos = ARFFparseString($line, $pos, $s, ",}");
 						if (is_string($pos)) {
-				      fclose($filehandle);
-						  return $pos . " (l.".$lineNumber.")";;
+							fclose($filehandle);
+							return $pos . " (l.".$lineNumber.")";
 						}
-						if ($s != '?' and $nominalValue[$i] != [])
+						$n = $featureType[$i];
+						if ($n == "numeric")
 						{
-							// check nominal attribute
-							if (array_search($s, $nominalValue[$i]) === FALSE) {
-				        fclose($filehandle);
-							  return "invalid nominal attribute value: " . $s . " (l.".$lineNumber.")";;
+							if ($s != '?' and ! is_numeric($s))
+							{
+								fclose($filehandle);
+								return "invalid value for numeric attribute: " . $s . " (l.".$lineNumber.")";
 							}
 						}
-						if ($pos == $len) {
-				      fclose($filehandle);
-						  return "closing brace missing in sparse data line" . " (l.".$lineNumber.")";;
+						else if ($n == "string")
+						{
+							// everything is allowed here
+							$nonnum++;
+						}
+						else if ($n == "date")
+						{
+							// date strings are non-trivial to validate, ignore
+							$nonnum++;
+						}
+						else
+						{
+							// check nominal attribute
+							assert(is_array($n));
+							if ($s != '?' and array_search($s, $n) === FALSE)
+							{
+								fclose($filehandle);
+								return "invalid value for nominal attribute: " . $s . " (l.".$lineNumber.")";
+							}
+							$nonnum++;
+						}
+
+						if ($pos == $len)
+						{
+							fclose($filehandle);
+							return "closing brace missing in sparse data line" . " (l.".$lineNumber.")";
 						}
 						if ($line[$pos] == '}') break;
 						$pos++;
 					}
+				}
+				if ($nonnum != $numberOfNonNumericFeatures)
+				{
+					fclose($filehandle);
+					return "at least one string, date, or nominal feature value is missing; this is disallowed also in sparse data format";
 				}
 			}
 			else
@@ -203,34 +241,35 @@ function ARFFcheck($filepath, $numLines = 100)
 				for ($i=0; $i<$p; $i++)
 				{
 					$pos = ARFFparseString($line, $pos, $s, ",");
-					if (is_string($pos)) {
-				    fclose($filehandle);
-					  return $pos . " (l.".$lineNumber.")";;
+					if (is_string($pos))
+					{
+						fclose($filehandle);
+						return $pos . " (l.".$lineNumber.")";
 					}
-					if ($s != '?' and $nominalValue[$i] != [])
+					$n = $featureType[$i];
+					if ($s != '?' and count($n) != 0)
 					{
 						// check nominal attribute
-						if (array_search($s, $nominalValue[$i]) === FALSE) {
-				      fclose($filehandle);
-						  return "invalid nominal attribute value: " . $s . " (l.".$lineNumber.")";;
+						if (array_search($s, $n) === FALSE)
+						{
+							fclose($filehandle);
+							return "invalid nominal attribute value: " . $s . " (l.".$lineNumber.")";
 						}
 					}
 					if ($line[$pos] == ',') $pos++;
 				}
-				if ($pos < $len) {
-				  fclose($filehandle);
-				  return "trailing characters on dense data line" . " (l.".$lineNumber.")";;
+				if ($pos < $len)
+				{
+					fclose($filehandle);
+					return "trailing characters on dense data line" . " (l.".$lineNumber.")";
 				}
 			}
 		}
-
 		$line = strtok("\n");
 		$count++;
-		if ($count == $numLines) break;   // for performance reasons we stop after 10 data lines
+		if ($count == $numLines) break;   // for performance reasons we stop after a fixed number of data lines
 	}
-
 	// judged as valid
 	return TRUE;
 }
-
 ?>
