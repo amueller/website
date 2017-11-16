d72891496177fdf2373ce3effb95d208c8317792
Andreas Mueller <andreas.mueller@columbia.edu>
2017-11-16 12:57:07 -0500

    change guide width

--- openml_OS/views/pages/frontend/guide/body.php
+++ openml_OS/views/pages/frontend/guide/body.php
@@ -1,6 +1,6 @@
 <div id="subtitle">Guide</div>
-<div class="container-fluid topborder endless guidecontainer openmlsectioninfo">
-  <div class="col-xs-12 col-md-10 col-md-offset-1 guidesection" id="mainpanel">
+<div class="container topborder endless guidecontainer openmlsectioninfo">
+  <div class="col-xs-10 col-md-8 col-md-offset-1 guidesection" id="mainpanel">
       <?php if(in_array($this->activepage,$this->activity_subpages) and false !== strpos($_SERVER['REQUEST_URI'],'/guide') ){
           subpage($this->activepage);
         } ?>
